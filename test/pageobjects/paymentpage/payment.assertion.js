dca67ccd15d4a5abdb52ccc50394db6c9af09b1f
amnazee <amnazee698@gmail.com>
2024-08-22 20:38:12 +0500

    Assignment Changes

--- test/pageobjects/paymentpage/payment.assertion.js
+++ test/pageobjects/paymentpage/payment.assertion.js
@@ -4,7 +4,7 @@ const paymentUI=require('../paymentpage/payment.locators')
 class PaymentAssertion{
     async paymentSuccessful(){
         await paymentTask.clickPay()
-        browser.pause(5000)
+        await browser.pause(5000)
         await paymentUI.PAYMENT_SUCCESS.waitForDisplayed()
         assert.equal("PAYMENT SUCCESS",await paymentUI.PAYMENT_SUCCESS.getText())
         await browser.pause(5000)
