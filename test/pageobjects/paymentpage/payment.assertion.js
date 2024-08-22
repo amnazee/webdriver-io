01de6df80412273fe6cf751f80ddbed422d222b5
amnazee <amnazee698@gmail.com>
2024-08-22 21:15:37 +0500

    Assignment Changes

--- test/pageobjects/paymentpage/payment.assertion.js
+++ test/pageobjects/paymentpage/payment.assertion.js
@@ -3,6 +3,14 @@ const paymentUI=require('../paymentpage/payment.locators')
 
 class PaymentAssertion{
     async paymentSuccessful(){
+        await browser.switchToParentFrame();
+        await browser.waitUntil(
+            async () => (await browser.execute(() => document.readyState)) === 'complete',
+            {
+                timeout: 20000,
+                timeoutMsg: 'Page did not load within 20 seconds'
+            }
+        );
         const heading=await browser.$("//h2")
         await heading.waitForDisplayed()
         assert.equal("PAYMENT SUCCESS",await paymentUI.paymentSuccess.getText())
